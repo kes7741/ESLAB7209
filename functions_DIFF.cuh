01b55e58f65c0fc99ce0804074c1658f58b5b0c6
So Hyun Park <bibirom10@gmail.com>
2018-02-27 13:21:38 +0900

    add diffusion model, rho_ref calculation
    1. add SPH diffusion model  : Artistodemo, Monaghan.
    2. add calculate reference density for salt-finger simulation (will be deleted).

--- functions_DIFF.cuh
+++ functions_DIFF.cuh
@@ -18,6 +18,37 @@ void Cuda_Particle_Array::predictor_concn(const Real dt)
 	KERNEL_clc_predictor_concn<<<number_of_particles,1>>>(dconcn,concn,concn0,dt);
 }
 //*/
+
+////////////////////////////////////////////////////////////////////////
+__global__ void KERNEL_clc_reference_density_diffusion(int_t nop,int_t tdim,part11*Pa11,part12*Pa12)
+{
+	int_t i=threadIdx.x+blockIdx.x*blockDim.x;
+	if(i>=nop) return;
+
+	//Real trhoA;
+	uint_t tp_type;
+	Real ttempi;
+	Real hi,vol, space;
+	Real tconcni;
+	Real rho_refi;
+	
+	tp_type=Pa11[i].p_type;
+	hi=Pa11[i].h;
+	ttempi=Pa11[i].temp;
+	tconcni=Pa12[i].concn;
+
+	space=hi/1.5;					// calculate initial particle spacing
+	vol=pow(space,tdim);			// calculate particle volume
+
+	//calculate reference density of mixed solution
+	//Real rho_w=1000.;
+	Real rho_w=reference_density(tp_type,ttempi,tconcni);
+	rho_refi=rho_w/(1-tconcni); 		// calculate salty water density using mass fraction(concn) of salt	
+	Pa11[i].rho_ref=rho_refi;
+	Pa11[i].m=rho_refi*vol;					// calculate salty water mass
+	//Pa11[i].rho_ref=reference_density(tp_type,ttemp,tconcn);
+	//Pa11[i].rho_ref=reference_density2(tp_type,ttemp,m,h,d);
+}
 ////////////////////////////////////////////////////////////////////////
 //Functions for concentration diffusion model
 __global__ void KERNEL_clc_concn_diffusion(int_t nop,int_t pnbs,part11*Pa11,part12*Pa12,part2*Pa2)
@@ -31,50 +62,45 @@ __global__ void KERNEL_clc_concn_diffusion(int_t nop,int_t pnbs,part11*Pa11,part
 	int_t j;																					// neighbor particle index-j
 	uint_t non;
 
-	Real mi,rhoi,rhoj;
+	Real mj,rhoi,rhoj;
 	Real diffi,diffj;
 	Real tempi,tempj;
 	uint_t p_typei,p_typej;
 	Real concni,concnj; //dconcn
-	Real tdist,hi;
-	Real xi,yi,zi,xj,yj,zj;
-	Real tdwx,tdwy,tdwz;
+	Real tdist,tdwij,hi;
+	//Real xi,yi,zi,xj,yj,zj;
+	//Real tdwx,tdwy,tdwz;
 
 	non=Pa11[i].number_of_neighbors;
 
 	if(cache_idx<non){
-		j=Pa2[tid].pnb;
-		tdist=Pa2[tid].dist;
-		tdwx=Pa2[tid].dwx;
-		tdwy=Pa2[tid].dwy;
-		tdwz=Pa2[tid].dwz;
+		tdist=Pa2[tid].dist;																	// distance between i and j particles (dist)
+		if(tdist>0.){
+			j=Pa2[tid].pnb;
+			tdwij=Pa2[tid].dwij;
+			
+			p_typei=Pa11[i].p_type;
+			tempi=Pa11[i].temp;
+			rhoi=Pa11[i].rho;
+			hi=Pa11[i].h;
 
-		p_typei=Pa11[i].p_type;
-		tempi=Pa11[i].temp;
-		mi=Pa11[i].m;
-		rhoi=Pa11[i].rho;
-		hi=Pa11[i].h;
-		xi=Pa11[i].x;
-		yi=Pa11[i].y;
-		zi=Pa11[i].z;
+			p_typej=Pa11[j].p_type;
+			tempj=Pa11[j].temp;
+			rhoj=Pa11[j].rho;
+			mj=Pa11[j].m;
 
-		p_typej=Pa11[j].p_type;
-		tempj=Pa11[j].temp;
-		rhoj=Pa11[j].rho;
-		xj=Pa11[j].x;
-		yj=Pa11[j].y;
-		zj=Pa11[j].z;
+			concni=Pa12[i].concn;																// concentration of particle i
+			concnj=Pa12[j].concn;																// concentration on particle j
 
-		concni=Pa12[i].concn;																// concentration of particle i
-		concnj=Pa12[j].concn;																// concentration on particle j
-
-		diffi=diffusion_coefficient(tempi,p_typei);				// diffusion coefficient of particle i
-		diffj=diffusion_coefficient(tempj,p_typej);			// diffusion coefficient of particle j
-
-		cache[cache_idx]=mi*(diffi*rhoi+diffj*rhoj)*((xi-xj)*tdwx+(yi-yj)*tdwy+(zi-zj)*tdwz)/(rhoi*rhoj*(tdist*tdist+0.01*hi*hi))*(concni-concnj)*(p_typei==p_typej);
+			diffi=diffusion_coefficient(tempi,p_typei);											// diffusion coefficient of particle i
+			diffj=diffusion_coefficient(tempj,p_typej);											// diffusion coefficient of particle j
+			
+			//cache[cache_idx]=mi*(diffi*rhoi+diffj*rhoj)/(rhoi*rhoj)*(concni-concnj)* tdwij*(tdistt+0.01*hi*hi));		// diffusion modeled by Aristodemo[2010]
+			cache[cache_idx]=4*mj/(rhoi*rhoj)*(diffi*diffj)/(diffi+diffj)*tdwij/(tdist+0.01*hi*hi)*(concni-concnj);		// diffusion modeled by Mongahan[2005]
+			
+		}
 	}
 	__syncthreads();
-
 	uint_t s;
 	for(s=blockDim.x*0.5;s>0;s>>=1){
 		if(cache_idx<s) cache[cache_idx]+=cache[cache_idx+s];
@@ -165,7 +191,6 @@ __global__ void KERNEL_clc_precor_update_concn(int_t nop,Real tdt,part12*Pa12)
 	tconcn0=Pa12[i].concn0;												// initial concentration
 	tdconcn_dt=Pa12[i].dconcn;										// time derivative of concentration
 	tconcnc=tconcn0+tdconcn_dt*(tdt*0.5);				// correct concentration
-	Pa12[i].concn=tconcnc;
 
 	// update concentration
 	Pa12[i].concn0=2*tconcnc-tconcn0;
